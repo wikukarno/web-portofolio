0ed2590e01d5f6ec2f8848916f5db3dabfca6a49
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-10-07 00:34:26 +0700

    Web Portofolio

---
+++ script/main.js
@@ -0,0 +1,25 @@
+// to top
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+
+// End to top
+
+// Hero typed
+if ($(".typed").length) {
+  var typed_strings = $(".typed").data("typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+  });
+}
